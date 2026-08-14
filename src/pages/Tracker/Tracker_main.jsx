a5a546b06eec9184625d9a91736ccab16cd4b589
hj1222 <hyunjeong12221222@gmail.com>
2026-08-14 21:31:54 +0900

    fix: App.jsx 충돌 정리 및 tracker 세팅

---
+++ src/pages/Tracker/Tracker_main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import '../../assets/tracker/scss/tracker_main.scss';
+
+
+function Tracker_main() {
+  return (
+    <div className='wrap'>
+      
+    </div>
+  )
+}
+
+export default Tracker_main
